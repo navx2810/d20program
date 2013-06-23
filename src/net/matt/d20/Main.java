10b19920957432c0500af125adff9c84f95ba6e2
Navx <navx2810@gmail.com>
2013-06-23 05:04:30 -0400

    added comments
    added some comments which described things.

--- src/net/matt/d20/Main.java
+++ src/net/matt/d20/Main.java
@@ -4,13 +4,13 @@ import net.matt.d20.net.matt.d20.utils.Items;
 
 public class Main {
 
-
+/* *****************************************************************
+ * For now this is a test app for the program. This will be replaced
+ * later with a working GUI version.
+ * *****************************************************************/
     public static void main(String[] args) {
 
         Character matt = new Character("Matt");
-        Character devin = new Character("Devin");
-        Campaign campaign = new Campaign("The legend of stuff");
-        PartyList partyList = new PartyList(campaign);
         Items longSword = new Items("Long Sword", "crappy wooden brand!");
 
         matt.addItem(longSword);
