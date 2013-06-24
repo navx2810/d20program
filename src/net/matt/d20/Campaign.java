54a1816e623ab4d657d37089fea77e8477432041
Navx <navx2810@gmail.com>
2013-06-23 21:11:29 -0400

    Updated Campaign  with new methods
    I added the Date and appropriate formatter to display when a session is
    created, and during each log. This will help players keep track of when.
    
    I changed the log to be a 'StringBuilder' instead of plain 'String';
    which will allow me to manipulate it easier then before.
    
    I have added stubs for beginSession() and endSession().
    
    I have added the appropriate JavaDoc information.

--- src/net/matt/d20/Campaign.java
+++ src/net/matt/d20/Campaign.java
@@ -1,26 +1,60 @@
 package net.matt.d20;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 /**
- * Creator: Matt Fetterman - 6/23/13
+ * This class is where the campaign log is held and manipulated
+ * @author Navx2810
+ * @version 1.0
+ * 
  */
+
 public class Campaign {
-    String name, log;
+    String name;
+    StringBuilder log;
+    Date date;
+    SimpleDateFormat monthDateFormat, timeDateFormat;
 
     // ----- [ Campaign constructor with NAME ] ----- //
-    
+    	
+    	/**
+    	 * the constructor for the campaign with the campaign's name
+    	 * @param name - the name of the campaign you're playing ex:"Village gone Fowl 1"
+    	 */
     public Campaign(String name) {
         this.name = name;
-        log = String.format("%s - Start\n", name);
+        date = new Date();
+        monthDateFormat = new SimpleDateFormat("MM/dd");
+        timeDateFormat = new SimpleDateFormat("hh:mm a");
+        
+        log = new StringBuilder();
+        log.append(String.format("Campaign: %s - Start [%s || %s]\n===========", name, monthDateFormat.format(date), timeDateFormat.format(date)) );
     }
-    
+    	
+    	/**
+    	 * this method will add a string to the log. It will automatically add any handlers (such as time + date) to the message log automatically
+    	 * @param msg - the message the player wants to record to the log.
+    	 */
     public void add(String msg){
-    	//do the stuff here
+    	log.append(String.format("\n[%s || %s] %s", monthDateFormat.format(date), timeDateFormat.format(date), msg));
     }
-    
-    public  String getLod() {
-		return null;
+    	
+    	/**
+    	 * this method will return a string of the entire log which will be used to save the campaign
+    	 * @return Log - the entire recorded summary of the current campaign.
+    	 */
+    public String getLog() {
+		return log.toString();
 	}
-    
-    //TODO: Create a getLog() function that will dump the entire log so it can be saved to a file
-
+    	// the function to use to continue a session that already has started
+    public void beginSession(){
+    	
+    }
+    	// ending a session
+    public void endSession(){
+    	
+    }
+   
 }
