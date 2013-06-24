ab2005277855d9b3cccd5fc2f6431464858e7dbe
Navx <navx2810@gmail.com>
2013-06-23 22:44:54 -0400

    Added the file saving

--- src/net/matt/d20/utils/CampaignSaver.java
+++ src/net/matt/d20/utils/CampaignSaver.java
@@ -2,6 +2,9 @@ package net.matt.d20.utils;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.PrintWriter;
+
+import net.matt.d20.Campaign;
 
 public class CampaignSaver {
 
@@ -10,6 +13,32 @@ public class CampaignSaver {
 		 * =========================================*/
 	
 	FileWriter fWriter;
+	PrintWriter pWriter;
 	File gameSave;
+	Campaign campaign;
+	String campaignName;
+	
+	/**
+	 * Constructor that starts the campaign saver
+	 * @param campaign - the campaign that's currently being played
+	 * @throws Exception - file not found
+	 */
+	public CampaignSaver(Campaign campaign) throws Exception{
+		this.campaign = campaign;
+		campaignName = campaign.name;
+		gameSave = new File(campaignName+".mtc");
+		
+		//if file exists, use FileWriter, if file doesn't, use PrintWriter
+		pWriter = new PrintWriter(gameSave);
+	
+	}
+	
+	/**
+	 * 
+	 */
+	public void saveCampaign() {
+		pWriter.print(campaign.getLog());
+		pWriter.close();
+	}
 	
 }
