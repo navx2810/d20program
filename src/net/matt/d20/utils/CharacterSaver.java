430ed5fb957b456aa2846b2dcc28fc12153af0a9
Navx <navx2810@gmail.com>
2013-07-01 02:37:08 -0400

    added CharacterSaver
    This class will handle individual characters as XML files. This will
    allow me to modulate character sheets to add new fields or remove them
    for future updates.

---
+++ src/net/matt/d20/utils/CharacterSaver.java
@@ -0,0 +1,11 @@
+package net.matt.d20.utils;
+
+/**
+ * this class will save characters as an XML file which will be separate from the campaign log.
+ * @author navx2_000
+ *
+ */
+
+public class CharacterSaver {
+
+}
